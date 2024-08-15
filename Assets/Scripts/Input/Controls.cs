26d137e514b613decd3501d8fc4aaafa105798a2
jvisvikis <69331363+jvisvikis@users.noreply.github.com>
2024-08-15 18:01:28 +1000

    Made a simple back and forth lasso indicator

--- Assets/Scripts/Input/Controls.cs
+++ Assets/Scripts/Input/Controls.cs
@@ -44,6 +44,15 @@ public partial class @Controls: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Aim"",
+                    ""type"": ""Value"",
+                    ""id"": ""6f17a8e2-6577-4263-ae98-911841a4f2eb"",
+                    ""expectedControlType"": ""Delta"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": true
                 }
             ],
             ""bindings"": [
@@ -112,6 +121,17 @@ public partial class @Controls: IInputActionCollection2, IDisposable
                     ""action"": ""Wrangle"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""374c997f-9f9b-4507-bb7c-a60c93849be7"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Aim"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -122,6 +142,7 @@ public partial class @Controls: IInputActionCollection2, IDisposable
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Wrangle = m_Player.FindAction("Wrangle", throwIfNotFound: true);
+        m_Player_Aim = m_Player.FindAction("Aim", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -185,12 +206,14 @@ public partial class @Controls: IInputActionCollection2, IDisposable
     private List<IPlayerActions> m_PlayerActionsCallbackInterfaces = new List<IPlayerActions>();
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Wrangle;
+    private readonly InputAction m_Player_Aim;
     public struct PlayerActions
     {
         private @Controls m_Wrapper;
         public PlayerActions(@Controls wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Player_Move;
         public InputAction @Wrangle => m_Wrapper.m_Player_Wrangle;
+        public InputAction @Aim => m_Wrapper.m_Player_Aim;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -206,6 +229,9 @@ public partial class @Controls: IInputActionCollection2, IDisposable
             @Wrangle.started += instance.OnWrangle;
             @Wrangle.performed += instance.OnWrangle;
             @Wrangle.canceled += instance.OnWrangle;
+            @Aim.started += instance.OnAim;
+            @Aim.performed += instance.OnAim;
+            @Aim.canceled += instance.OnAim;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -216,6 +242,9 @@ public partial class @Controls: IInputActionCollection2, IDisposable
             @Wrangle.started -= instance.OnWrangle;
             @Wrangle.performed -= instance.OnWrangle;
             @Wrangle.canceled -= instance.OnWrangle;
+            @Aim.started -= instance.OnAim;
+            @Aim.performed -= instance.OnAim;
+            @Aim.canceled -= instance.OnAim;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -237,5 +266,6 @@ public partial class @Controls: IInputActionCollection2, IDisposable
     {
         void OnMove(InputAction.CallbackContext context);
         void OnWrangle(InputAction.CallbackContext context);
+        void OnAim(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/Input/PlayerController.cs
+++ Assets/Scripts/Input/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
@@ -8,7 +9,13 @@ public class PlayerController : MonoBehaviour
     private Controls playerControls;
     private Rigidbody2D rb2d;
 
-    [SerializeField] float speed = 2f;
+    [SerializeField] private GameObject lasso;
+    [SerializeField] private float lassoRange = 5f;
+    [SerializeField] private float lassoChargeTime = 1f;
+    [SerializeField] private float speed = 2f;
+    
+    private float lassoTimer;
+    private bool lassoing;
 
     void Awake()
     {
@@ -31,8 +38,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(GetDirection() * speed);
-        rb2d.velocity = GetDirection() * speed ;
+        if(!lassoing) rb2d.velocity = GetDirection() * speed;
+        else rb2d.velocity = Vector2.zero;
     }
 
     Vector2 GetDirection()
@@ -43,10 +50,28 @@ public class PlayerController : MonoBehaviour
     void StartLassoCharge()
     {
         Debug.Log("Start Lasso");
+        lassoing = true;
+        StartCoroutine(ChargeLasso());
     }
 
     void ReleaseLasso()
     {
         Debug.Log("Release Lasso");
+        lassoing = false;
+    }
+
+    public IEnumerator ChargeLasso()
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();   
+        mousePos.z = Camera.main.nearClipPlane;
+        Vector3 Worldpos=Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2 dir = new Vector2(Worldpos.x, Worldpos.y).normalized;
+        while(lassoing)
+        {
+            lasso.transform.localPosition = Vector2.Lerp(Vector2.zero, dir*lassoRange, Mathf.PingPong(lassoTimer/lassoChargeTime,1));
+            lassoTimer += Time.deltaTime;
+            yield return null;
+        }
+        lassoTimer = 0;
     }
 }
