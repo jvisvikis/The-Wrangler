17a6340f555f20ac13a71218caea7c9e9e74c46c
Benjamin Kalman <btkalman@gmail.com>
2024-08-30 18:01:46 +1000

    Use animation events for footstep sounds

---
+++ Assets/Scripts/Audio/FMODAnimationEventEmitter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODAnimationEventEmitter : MonoBehaviour
+{
+    [SerializeField]
+    FMODUnity.StudioEventEmitter eventEmitter;
+
+    [SerializeField]
+    FMODUnity.StudioEventEmitter eventEmitterStart;
+
+    [SerializeField]
+    FMODUnity.StudioEventEmitter eventEmitterStop;
+
+    public void Emit(AnimationEvent animationEvent)
+    {
+        if (IsFirstOfLoop(animationEvent))
+        {
+            eventEmitterStart.Play();
+        }
+        else if (animationEvent.animatorClipInfo.weight == 1f)
+        {
+            eventEmitter.Play();
+        }
+    }
+
+    public void Idle(AnimationEvent animationEvent)
+    {
+        if (IsFirstOfLoop(animationEvent))
+        {
+            eventEmitterStop.Play();
+        }
+    }
+
+    bool IsFirstOfLoop(AnimationEvent animationEvent)
+    {
+        return animationEvent.animatorStateInfo.normalizedTime < 0.1f;
+    }
+}
