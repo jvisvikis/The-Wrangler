a3a43126adc3e43f0ac1e441c90467600921b39f
jvisvikis <69331363+jvisvikis@users.noreply.github.com>
2024-08-31 22:24:01 +1000

    Added controls panel

---
+++ Assets/MenuUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuUI : MonoBehaviour
+{
+    [SerializeField] private GameObject controlsPanel;
+
+    void Start()
+    {
+        controlsPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Mouse.current.leftButton.isPressed && controlsPanel.activeSelf)
+            SetControlsPanel(false);
+
+    }
+
+    public void SetControlsPanel(bool active)
+    {
+        controlsPanel.SetActive(active);
+    }
+}
